9851d13f73a5bff93209e32efbdf225174af8b9e
lisa-vong <lisa.vongg0@gmail.com>
2023-09-18 16:17:07 -0400

    player draw

--- script.js
+++ script.js
@@ -13,4 +13,18 @@ class Player{
         this.velocity = velocity;
     }
     
+    draw(){
+        c.moveTo(this.position.x + 30, this.position.y);
+        c.lineTo(this.position.x - 10, this.position.y -10);
+        c.lineTo(this.position.x - 10, this.position.y + 10);
+        c.closePath();
+
+        c.strokeStyle = "white";
+        c.stroke();
+    }
 }
+
+const player = new Player({
+    position: {x: canvas.width/2, y: canvas.height/2},
+    velocity: {x: 0, y: 0}
+})
