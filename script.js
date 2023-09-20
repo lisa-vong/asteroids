4dc25a40670d28ad8ddb602268b66c6fd6ae5a15
lisa-vong <lisa.vongg0@gmail.com>
2023-09-20 00:28:14 -0400

    rotate player cw

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ class Player{
     constructor ({position, velocity}){
         this.position = position;
         this.velocity = velocity;
+        this.rotation = 0;
     }
     
     draw(){
+        c.save();
+
+        c.translate(this.position.x, this.position.y);
+        c.rotate(this.rotation);
+        c.translate(-this.position.x, -this.position.y);
+
         c.beginPath();
         c.moveTo(this.position.x + 30, this.position.y);
         c.lineTo(this.position.x - 10, this.position.y -10);
@@ -19,6 +26,8 @@ class Player{
 
         c.strokeStyle = "white";
         c.stroke();
+
+        c.restore();
     }
 
     update(){
@@ -54,9 +63,15 @@ function animate(){
 
     player.update();
 
+    player.velocity.x = 0;
     if (keys.w.pressed){
         player.velocity.x = 1;
     }
+
+    if (keys.d.pressed){
+        player.rotation += 0.01;
+    }
+
 }
 
 animate();
@@ -71,6 +86,20 @@ window.addEventListener("keydown", (event) => {
             break;
         case "KeyD":
             keys.d.pressed = true;
+            break;
+    }
+})
+
+window.addEventListener("keyup", (event) => {
+    switch (event.code){
+        case "KeyW":
+            keys.w.pressed = false;
+            break;
+        case "KeyA":
+            keys.a.pressed = false;
+            break;
+        case "KeyD":
+            keys.d.pressed = false;
             break;
     }
 })
